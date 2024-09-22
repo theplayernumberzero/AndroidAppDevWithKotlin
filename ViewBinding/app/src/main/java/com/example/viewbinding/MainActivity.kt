ba3174a00643fab90c3b70bce44f4af2a402c1fb
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-09-22 22:26:21 +0300

    Kotlin Basics: ViewBinding

---
+++ ViewBinding/app/src/main/java/com/example/viewbinding/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.viewbinding
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.viewbinding.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainBinding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val view = mainBinding.root
+
+        setContentView(view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        mainBinding.buttonOk.setOnClickListener {
+            mainBinding.textViewResult.text = mainBinding.editTextName.text.toString()
+        }
+    }
+}
