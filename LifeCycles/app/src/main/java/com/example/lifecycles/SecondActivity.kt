27dc0105ee71abc3c88173f9caf0fed97c955ff1
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-09-22 12:52:53 +0300

    Kotlin Basics: Activity Lifecycle

--- LifeCycles/app/src/main/java/com/example/lifecycles/SecondActivity.kt
+++ LifeCycles/app/src/main/java/com/example/lifecycles/SecondActivity.kt
@@ -4,70 +4,62 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-    lateinit var text1 : TextView
-    lateinit var buttonAdd : Button
-    lateinit var buttonNext : Button
+class SecondActivity : AppCompatActivity() {
 
-    var count : Int = 0
+    lateinit var buttonBack : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        Log.d("Message", "First Activity onCreate worked..")
+        buttonBack = findViewById(R.id.buttonBack)
 
-        buttonAdd.setOnClickListener {
+        Log.d("Message", "Second Activity onCreate worked..")
 
-            count++
-            text1.setText("$count")
-        }
-
-        buttonNext.setOnClickListener {
-
-            var intent = Intent(this@MainActivity, SecondActivity::class.java)
+        buttonBack.setOnClickListener {
+            var intent = Intent(this@SecondActivity, MainActivity::class.java)
             startActivity(intent)
         }
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d("Message", "First Activity onStop worked..")
+        Log.d("Message", "Second Activity onStop worked..")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d("Message", "First Activity onStart worked..")
+        Log.d("Message", "Second Activity onStart worked..")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d("Message", "First Activity onPause worked..")
+        Log.d("Message", "Second Activity onPause worked..")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d("Message", "First Activity onResume worked..")
+        Log.d("Message", "Second Activity onResume worked..")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.d("Message", "First Activity onRestart worked..")
+        Log.d("Message", "Second Activity onRestart worked..")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d("Message", "First Activity onDestroy worked..")
+        Log.d("Message", "Second Activity onDestroy worked..")
     }
 }
