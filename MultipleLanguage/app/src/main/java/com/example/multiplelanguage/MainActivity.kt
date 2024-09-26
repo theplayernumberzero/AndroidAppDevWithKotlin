b7b3542db9a5756f831284e48224121b794b4164
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-09-26 17:39:00 +0300

    Kotlin Basics: Multiple Language

---
+++ MultipleLanguage/app/src/main/java/com/example/multiplelanguage/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.multiplelanguage
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var showButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        showButton = findViewById(R.id.button)
+
+        showButton.setOnClickListener {
+            Toast.makeText(applicationContext, R.string.toast, Toast.LENGTH_LONG).show()
+        }
+    }
+}
