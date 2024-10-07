a32ef299ad29d40dae44e80d666d130fbcbb60ad
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-10-07 23:08:23 +0300

    Kotlin Projects: Flag Quiz App - Splash Screen

---
+++ FlagQuizApp/app/src/main/java/com/example/flagquizapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.flagquizapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        //Belirlenen süre içerisinde uygulamanın geç açılmasını sağlar
+        Thread.sleep(3000L)
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
