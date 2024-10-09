d55ce0c77c5ff949b054298a58299d5d8e690d37
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-10-09 15:05:14 +0300

    Kotlin Projects: Flag Quiz App - Navigation

--- FlagQuizApp/app/build.gradle.kts
+++ FlagQuizApp/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("androidx.navigation.safeargs")
 }
 
 android {
@@ -40,6 +41,8 @@ android {
 
 dependencies {
 
+    val nav_version = "2.8.2"
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -49,5 +52,10 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 
+    //Chart
     implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
+    //Navigation
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
 }
