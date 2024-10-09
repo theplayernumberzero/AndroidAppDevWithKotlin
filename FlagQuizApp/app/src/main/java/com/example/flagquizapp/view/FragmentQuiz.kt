d821ccebfbd65c72b47fd69585e68ef9510bd723
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-10-09 13:40:02 +0300

    Kotlin Projects: Flag Quiz App - Quiz Page

--- FlagQuizApp/app/src/main/java/com/example/flagquizapp/view/FragmentQuiz.kt
+++ FlagQuizApp/app/src/main/java/com/example/flagquizapp/view/FragmentQuiz.kt
@@ -6,15 +6,35 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.flagquizapp.R
+import com.example.flagquizapp.databinding.FragmentQuizBinding
 
 class FragmentQuiz : Fragment() {
 
+    lateinit var fragmentQuizBinding: FragmentQuizBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        fragmentQuizBinding = FragmentQuizBinding.inflate(inflater, container, false)
+
+        fragmentQuizBinding.buttonA.setOnClickListener {
+
+        }
+        fragmentQuizBinding.buttonB.setOnClickListener {
+
+        }
+        fragmentQuizBinding.buttonC.setOnClickListener {
+
+        }
+        fragmentQuizBinding.buttonD.setOnClickListener {
+
+        }
+        fragmentQuizBinding.buttonNext.setOnClickListener {
+
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        return fragmentQuizBinding.root
     }
 
 }
